7f8272ebf578f76b276635578ddbeaa3d37be253
DanKair <kair.daniyaruyl@gmail.com>
2025-10-31 21:40:20 +0500

    Bank Accounts Lab5 added;

---
+++ OOP/lab5/src/BankAccounts/DepositAccount.java
@@ -0,0 +1,10 @@
+package BankAccounts;
+
+public class DepositAccount extends Account {
+
+    public DepositAccount(String owner, double balance){
+        super(owner, balance, "Deposit");
+    }
+
+
+}
